354991a3916db7af3b3286a4bad733f72a69d388
saGen <sagensakam015@gmail.com>
2018-09-28 12:22:03 +0530

    Fix the errors

--- app/src/main/java/com/soren/sagen/bluetoothdemo/MainActivity.java
+++ app/src/main/java/com/soren/sagen/bluetoothdemo/MainActivity.java
@@ -1,84 +1,73 @@
 package com.soren.sagen.bluetoothdemo;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
-
-    BluetoothAdapter BA;
-
-
-    public void turnBluetoothOff(View view){
-
-        BA.disable();
-        if (BA.isEnabled()){
-
-            Toast.makeText(getApplicationContext(),"Bluetooth could not disabled",Toast.LENGTH_LONG).show();
-
-        }else {
-
-            Toast.makeText(getApplicationContext(),"Bluetooth turned off",Toast.LENGTH_LONG).show();
-
-        }
-    }
+public class MainActivity extends Activity  {
+    Button b1,b2,b3,b4;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice>pairedDevices;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (BA.isEnabled()){
+        b1 = findViewById(R.id.button);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b4=findViewById(R.id.button4);
 
-            Toast.makeText(getApplicationContext(),"Bluetooth is on",Toast.LENGTH_LONG).show();
+        BA = BluetoothAdapter.getDefaultAdapter();
+        lv = findViewById(R.id.listView);
+    }
 
-        }else {
-            Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(i);
-
-            if (BA.isEnabled()){
-
-                Toast.makeText(getApplicationContext(),"Bluetooth turned on",Toast.LENGTH_LONG).show();
-
-            }
+    public void on(View v){
+        if (!BA.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
         }
     }
 
-
-
-
-    public void findDiscoverableDevice(View view){
-        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivity(intent);
+    public void off(View v){
+        BA.disable();
+        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
     }
 
-    public void pairedDevice(View view){
 
-        Set<BluetoothDevice> pairedDevices = BA.getBondedDevices();
-
-        ListView listView = findViewById(R.id.paired_device_lv);
-
-        ArrayList pairedDeviceArrayList = new ArrayList();
-
-        for (BluetoothDevice bluetoothDevice : pairedDevices){
-
-            pairedDeviceArrayList.add(bluetoothDevice.getName());
-
-
-        }
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,pairedDeviceArrayList);
-
-        listView.setAdapter(arrayAdapter);
-
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
     }
 
+
+    public void list(View v){
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
+    }
 }
